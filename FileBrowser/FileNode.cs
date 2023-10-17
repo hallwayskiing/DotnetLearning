4485978f73a416120276b4291e932959a2c4d514
hallwayskiing <541814852@qq.com>
2023-10-18 02:02:45 +0800

    4th_homework

---
+++ FileBrowser/FileNode.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace FileBrowser
+{
+    public class FileNode
+    {
+        public string FileName {  get; set; }
+        public string LastAccess {  get; set; }
+        public string Path {  get; set; }
+
+        private string type;
+        public string Type
+        {
+            get { return type; }
+            set { type = value; ImageSource = $"/icons/{value}.png";}
+        }
+        public string ImageSource {  get; private set; }
+        public long Size {  get; set; }
+        public List<FileNode> Children { get; set; }
+        
+        public FileNode()
+        {
+            FileName=string.Empty;
+            LastAccess=string.Empty;
+            Path=string.Empty;
+            type=string.Empty;
+            ImageSource=string.Empty;
+            Size = 0;
+            Children = new List<FileNode>();
+        }
+    }
+}
---
+++ FileBrowser/MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Path = System.IO.Path;
+
+namespace FileBrowser
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<FileNode>obsList= new ObservableCollection<FileNode>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            FileNode node = new()
+            {
+                FileName = "C:",
+                Path = "C:",
+                Type = "disk"
+            };
+            fileTree.Items.Add(node);
+            fileList.ItemsSource = obsList;
+        }
+
+        private void FileTreeItem_Selected(object sender, RoutedEventArgs e)
+        {
+            try
+            {               
+                var item = (TreeViewItem)sender;
+                var node = (FileNode)item.DataContext;
+                obsList.Clear();
+                var list = new DirectoryInfo(node.Path).GetFileSystemInfos();
+                foreach (var file in list)
+                {
+                    var child = new FileNode
+                    {
+                        FileName = file.Name,
+                        Path = file.FullName,
+                        Type = file.Extension switch
+                        {
+                            ".exe" => "exe",
+                            ".txt" => "txt",
+                            "" => "folder",
+                            _ => "other",
+                        },
+                        LastAccess = file.LastAccessTime.ToShortDateString()+","+ file.LastAccessTime.ToLongTimeString(),                    
+                        Size=File.Exists(file.FullName)?new FileInfo(file.FullName).Length:0,
+                    };
+                    node.Children.Add(child);
+                    obsList.Add(child);
+                }
+                item.IsExpanded = true;
+                e.Handled = true;
+            }
+            catch (UnauthorizedAccessException)
+            {
+                MessageBox.Show("Access Denied!");
+            }
+            catch { }
+        }
+
+        private void FileTreeItem_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                var item = (TreeViewItem)sender;
+                var node = (FileNode)item.DataContext;
+
+                switch (node.Type)
+                {
+                    case "txt":
+                        Process.Start("notepad.exe", node.Path);
+                        break;
+                    case "exe":
+                        Process.Start(node.Path);
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
